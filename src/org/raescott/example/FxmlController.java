b6b62803df5c1f955a37d14ed2f911b27341c861
Richard Scott Smith <scott.smith@isostech.com>
2018-05-18 18:59:30 -0700

    Added the button and action which displays the text.

--- src/org/raescott/example/FxmlController.java
+++ src/org/raescott/example/FxmlController.java
@@ -1,7 +1,17 @@
 package org.raescott.example;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 /**
  * @author Richard Scott Smith <pilbender@gmail.com>
  */
 public class FxmlController {
+	@FXML
+	private Text actiontarget;
+
+	@FXML protected void handleSubmitButtonAction(ActionEvent event) {
+		actiontarget.setText("Sign in button pressed");
+	}
 }
